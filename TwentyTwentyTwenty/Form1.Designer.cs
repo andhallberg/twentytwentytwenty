e9f50e9df5a335825a63c51883ab1ee7b363b0dc
Andreas Hallberg <andhallberg@gmail.com>
2021-06-03 13:11:55 +0200

    First commit

---
+++ TwentyTwentyTwenty/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace TwentyTwentyTwenty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vila ögonen - 20m, 20s";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 82);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TwentyTwentyTwenty/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwentyTwentyTwenty.Properties;
+
+namespace TwentyTwentyTwenty
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyCustomApplicationContext());
+        }
+    }
+
+    public class MyCustomApplicationContext : ApplicationContext
+    {
+        private static readonly TimeSpan HideTime = TimeSpan.FromMinutes(20);
+        private static readonly TimeSpan ShowTime = TimeSpan.FromSeconds(20);
+
+        private readonly NotifyIcon trayIcon;
+        private readonly Form1 form = new Form1();
+
+        public MyCustomApplicationContext()
+        {
+            trayIcon = new NotifyIcon
+            {
+                Icon = Resources.AppIcon,
+                ContextMenu = new ContextMenu(new[] { new MenuItem("Exit", Exit) }),
+                Visible = true
+            };
+
+            var uiThreadSyncContext = SynchronizationContext.Current;
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(HideTime);
+                    uiThreadSyncContext.Post(state => form.Visible = true, null);
+                    Thread.Sleep(ShowTime);
+                    uiThreadSyncContext.Post(state => form.Visible = false, null);
+                }
+                // ReSharper disable once FunctionNeverReturns
+            });
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+    }
+}
